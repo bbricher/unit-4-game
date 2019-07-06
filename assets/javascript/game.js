359c0962f5401a5f6ea0f309389c8e4794abdff5
Briana Bricher <bbricher@udallas.edu>
2019-07-05 20:32:50 -0500

    created if statements and completed game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,26 @@
 /* 1. create variables */
 
-var randomNumberGenerated = Math.floor(Math.random() * 100) + 1; 
+var randomNumberGenerated = ""; 
 
 console.log(randomNumberGenerated);
 
-var gemNumber = [5, 2, 1, 10, 20, 3]
+var gemNumber = [5, 2, 1, 10]
 
-var redGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+var redGemNumberGenerated = "";
 
-var blueGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+var blueGemNumberGenerated = "";
 
-var yellowGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+var yellowGemNumberGenerated = "";
 
-var greenGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+var greenGemNumberGenerated = "";
 
 
-var win = 0;
-var lose = 0;
+var win = 1;
+var lose = 1;
 
 var totalScore = 0;
 
-var numberGoalElement = $("#number-goal")
+var numberGoalElement = $("#number-goal");
 
 var redGemElement = $("#red-gem");
 
@@ -30,13 +30,23 @@ var yellowGemElement = $("#yellow-gem");
 
 var greenGemElement = $("#green-gem");
 
-var totalScoreElement = $("#total-number")
+var totalScoreElement = $("#total-number");
+
+var winElement = $("#win");
+
+var loseElement = $("#lose");
+
 /* 2. create start game logic */
 
 function startGame() {
+    randomNumberGenerated = Math.floor(Math.random() * 100) + 1;
     numberGoalElement.text(randomNumberGenerated);
     totalScore = 0;
     totalScoreElement.text(totalScore); 
+    redGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+    blueGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+    yellowGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
+    greenGemNumberGenerated = gemNumber[Math.floor(Math.random() * gemNumber.length)];
     redGemElement.val(redGemNumberGenerated);
     blueGemElement.val(blueGemNumberGenerated);
     yellowGemElement.val(yellowGemNumberGenerated);
@@ -49,23 +59,26 @@ function startGame() {
 startGame();
 
 $(document).ready(function() {
+    
+    var randomNumberGeneratedInt = parseInt(randomNumberGenerated);
 
     $(".button-gem").on("click", function() {
         var totalScoreInt = parseInt(totalScore);
         var gemValueInt = parseInt($(this).val());
         totalScore = totalScoreInt + gemValueInt;
         totalScoreElement.text(totalScore);
+        console.log(totalScore);
+        if (randomNumberGeneratedInt === totalScore) {
+            alert("Good job! You get a point!")
+            winElement.text(win++);
+            startGame();
+            } else if (randomNumberGeneratedInt < totalScore) {
+            alert("Sorry! Try again!");
+            loseElement.text(lose++);
+            startGame();
+            }
+    
     })
-
-
-
-
-
-
-
-
-
-
 
 });
 /* 5. create logic to add up button clicks */
